0e3159fa16d52533c3bcaec80b4c34bc2773d8e1
TolRiaz <34152412+TolRiaz@users.noreply.github.com>
2019-07-08 18:22:08 +0900

    Update wget.sh

--- client/wget.sh
+++ client/wget.sh
@@ -5,9 +5,7 @@ timer=$1
 
 while [ $i -ne $timer ]
 do
-        time wget -r $2
-#       index=time wget -r $2
-#       echo "$index" >> index.html
+        (time wget -r $2) 2>> index.html
         sleep 1s
         i=$(($i+1))
 done
