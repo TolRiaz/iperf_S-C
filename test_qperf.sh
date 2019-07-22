5b3434ed025c60b566e21d8796663747bb711372
TolRiaz <34152412+TolRiaz@users.noreply.github.com>
2019-07-22 14:53:17 +0900

    Update test_qperf.sh

--- test_qperf.sh
+++ test_qperf.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
-BW=$1
 timer=$2
 currentBW=0
 currentCNT=0
-let term=$timer*4
+let BW=$1
 
-while [ $currentBW -ne $BW ]
+while [ $currentBW -le $BW ]
 do
         currentCNT=$(($currentCNT+1))
         if [ "$currentCNT" = $timer ]; then
+                # kill iperf
+                kill -9 $(ps -ef | grep "iperf -c" | grep -v grep | awk '{print $2}') &
                 currentCNT=0
-                currentBW=$(($currentBW+2))
-                iperf -c 220.149.13.181 -u -b$(($currentBW))M -t $term &
+                currentBW=$(($currentBW+1))
+                iperf -c 220.149.13.181 -u -b$(($currentBW))M -t 300 &
         fi
 
         qperf 220.149.13.181 --listen_port 10110 tcp_bw tcp_lat >> qperf_test_$(($currentBW))M
