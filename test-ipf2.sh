7af011bc4b44863a606c1028662cd69a38618039
TolRiaz <34152412+TolRiaz@users.noreply.github.com>
2019-11-03 21:15:23 +0900

    Update test-ipf2.sh

--- test-ipf2.sh
+++ test-ipf2.sh
@@ -7,8 +7,19 @@ else
         ipf="iperf -c $1 -u -i1 -b$2M -p $3 -P $4 -t 5"
 fi
 
+FILE=Check
+
 while true
 do
         { $ipf | grep 'read failed: Connection refused';
+
+        FILESIZE=$(wc -c "$FILE" | awk '{print $1}')
+
+        if [ $FILESIZE -eq 0 ]; then    #Check file
+                echo OK
+        else
+                echo Disconnected
+        fi
+
         sleep 1
 done
